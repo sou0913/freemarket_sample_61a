31f2331f2bb8453aedb601812edd22f4b125067b
sou0913 <usokisasa0913@gmail.com>
2019-10-28 20:16:40 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 # working_directory "#{app_path}/current"
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 #以下１行はnginx導入カリキュラムでパスを変更している
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
