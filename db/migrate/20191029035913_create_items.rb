65209ccd19400af170cf93f084a7b1f7e83acf6f
“sakai51” <stakashi51@gmail.com>
2019-10-29 15:23:41 +0900

    migration edit

--- db/migrate/20191029035913_create_items.rb
+++ db/migrate/20191029035913_create_items.rb
@@ -1,7 +1,19 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.integer :price,           null: false
+      t.string  :description,     null: false
+      t.string  :title
+      t.references :category,     null: false, foreign_key: true
+      t.integer :status,          null: false
+      t.integer :region,          null: false
+      t.integer :shipping_charge, null: false
+      t.integer :delivery_source, null: false
+      t.integer :shipping_day,    null: false
+      t.integer :shipping_method, null: false
+      t.integer :dealing,         null: false, default: 0
+      t.references :user,         null: false, foreign_key: true
+      t.references :brand,        null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20191029040128_create_cards.rb
+++ db/migrate/20191029040128_create_cards.rb
@@ -1,7 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-
+      t.integer :number,           null: false
+      t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
