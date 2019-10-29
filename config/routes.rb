83232a4cfe142d5264b149231d80e5ce9b0a80f4
sou0913 <usokisasa0913@gmail.com>
2019-10-29 19:19:37 +0900

    create frameworks first

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tests, only: :index
-  root to: "tests#index"
+  resources :tests, only: [:index, :new]
+  root to: "tests#new"
 end
