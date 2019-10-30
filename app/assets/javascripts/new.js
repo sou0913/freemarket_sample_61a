10f4939cb4ed441e635ded71c8bbc7e58d8294c5
sou0913 <usokisasa0913@gmail.com>
2019-10-30 16:02:59 +0900

    fix views

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,5 +1,6 @@
 $(function(){
   var drag_space = $(".exhibit-wrapper__main__image__drag")
+  var drag_space_form = $(".exhibit-wrapper__main__image__drag__form")
   drag_space.on('dragenter', function(e){
     e.preventDefault();
     $(this).css("border", "2px solid rgba(204,204,204,0.9)");
@@ -8,10 +9,20 @@ $(function(){
     e.preventDefault();
   });
   drag_space.on('drop', function(e){
-    $(this).css('broder', '2px dotted rgba(204,204,204,0.9)');
+    $(this).css('border', '2px dotted rgba(204,204,204,0.9)');
     e.preventDefault();
-    var file_field = $("#exhibit__file-form");
     document.getElementById("exhibit__file-form").files = e.originalEvent.dataTransfer.files;
-    
   });
+  drag_space_form.on("change", function(e){
+    var id = drag_space.data("exhibitid");
+    $(".exhibit-wrapper__main__image__cards__card").eq(Number(id)).addClass("exhibit-active");
+    var newid = Number(id) + 1
+    drag_space.attr("data-exhibitid", newid);
+    var reader = new FileReader();
+    reader.onload = function(e){
+      $(`.exhibit-image-${Number(id)}`).attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  })
 })
+  //   
