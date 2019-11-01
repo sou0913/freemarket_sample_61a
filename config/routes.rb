ca69db8fa0f0c57ffa5b0d8120381ba0e636b92c
minori <minori.ap.wan@gmail.com>
2019-11-01 15:20:39 +0900

    fix css misalignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: "users/sessions",
   } 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :private_informations, only: [:new]
+  end
   resources :items do
     resources :purchases
   end
