cb268992a771f43fdbf451622008d378513fddea
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-01 14:16:31 +0900

    update to item/show.scss

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+  
+  def show
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tests, only: :index
+  resources :tests, only: :index 
+  resources :items
   root to: "tests#index"
 end
