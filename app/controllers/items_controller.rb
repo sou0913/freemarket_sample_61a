e02bf2445fbf1845cc14b32f7ffaded59acbbbe6
sou0913 <usokisasa0913@gmail.com>
2019-11-01 13:01:18 +0900

    [create] uploading images but not yet.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.images.build
   end
   def create
     binding.pry
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ApplicationRecord
+  
+  belongs_to :item
+  mount_uploader :image, ImageUploader
+  
 end
