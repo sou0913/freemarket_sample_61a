215b1140c7a70f266b16a4bcda8233e0be7c8557
sou0913 <usokisasa0913@gmail.com>
2019-11-05 13:16:26 +0900

    be able to delete images.

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,16 +1,17 @@
 $(document).on('turbolinks:load', function(){
   var controller = $('body').data("controller");
   var action = $('body').data("action");
-  var counter = $(".exhibit-wrapper__main__image__cards").data("exhibitid");
+  var counter = $(".exhibit-wrapper__main__image__cards");
   try {
     if (controller != "items" || action != "edit"){throw new Error(e)}
-  function buildHTML(img){ 
+  function buildHTML(img, number){ 
   html = `<div class="exhibit-wrapper__main__image__cards__card">
             <div class="exhibit-wrapper__main__image__cards__card__image">
               <img src= ${img} class: 'exhibit-image'>
+            </div>
             <div class="exhibit-wrapper__main__image__cards__card__button">
-              <a href="#">編集</a> 
-              <a href="#">削除</a>
+              <p>編集(仮)</p> 
+              <p class="item-image__delete-button__append" data-image=${number}>削除</p>
             </div>
           </div>`;
   return html;
@@ -18,21 +19,26 @@ $(document).on('turbolinks:load', function(){
   window.onload = function() {
     for (var i=0; i<10; i++) {
       if (document.getElementById(`item_images_attributes_${i}_id`) != null) {
-        counter += 1;
+        var number = counter.attr("data-exhibitid");
+        var new_number = Number(number)   + 1;
+        counter.attr("data-exhibitid", new_number);
         $(`#item_images_attributes_${i}_image`).prop('readonly', true);
         $(`#item_images_attributes_${i}_image`).next().prop('readonly', false);
-        $("#item_images_attributes_9_image").after($(`#item_images_attributes_${i}_image`));
+        // 新規出品とは違い、formが再利用されないようにアンカーの後ろに移動
+        $(".exhibit-wrapper__main__image__cards_anchor").after($(`#item_images_attributes_${i}_image`));
+        $(`#item_images_attributes_${i}_image`).after($(`#item_images_attributes_${i}__destroy`));
         // 二段目に移行する処理
-        if ($(`#item_images_attributes_${i}_image`).get(0) == $("#item_images_attributes_4_image").get(0)) {
+        if (counter.attr("data-exhibitid") == 5) {
           $(".exhibit-wrapper__main__image__cards").css("height", "320px");
         }
         var imgUrl = $(`.item-edit__image-${i}`).attr("src");
-        addCard = buildHTML(imgUrl);
+        addCard = buildHTML(imgUrl, i);
         $(".exhibit-wrapper__main__image__cards").prepend(addCard);
-        if (counter == 10) {
+        if (counter.attr("data-exhibitid") == 10) {
           $(".exhibit-wrapper__main__image__drag-wrapper").css("display", "none");
         }
       }
+
     }
   }
   }catch(e){}
