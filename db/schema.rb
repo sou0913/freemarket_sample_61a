04fdad5706b0f6eb58867ea6e778ecd03de30755
“sakai51” <stakashi51@gmail.com>
2019-11-06 17:42:35 +0900

    commit edit name colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_055001) do
+ActiveRecord::Schema.define(version: 2019_11_06_084102) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2019_11_04_055001) do
   end
 
   create_table "private_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "family_kana", null: false
     t.string "first_kana", null: false
     t.date "birthday", null: false
@@ -85,6 +84,8 @@ ActiveRecord::Schema.define(version: 2019_11_04_055001) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name"
+    t.string "first_name"
     t.index ["user_id"], name: "index_private_informations_on_user_id"
   end
 
