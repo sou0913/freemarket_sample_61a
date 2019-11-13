0feaed838bbc790fc4391a1f92155330c035c0ff
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-13 19:26:59 +0900

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2019_11_13_050319) do
 
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_kana", null: false
+    t.string "first_kana", null: false
+    t.string "postal_code", null: false
+    t.string "prefectures", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building_name", null: false
+    t.string "phone_number", null: false
+    t.string "user", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -94,7 +110,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_050319) do
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
-    t.string "phone_number", null: false
+    t.string "phone_number"
     t.string "phone_number_deliver"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
