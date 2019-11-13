f916af234b880bd1cdfa82583d705fd5beade22f
“sakai51” <stakashi51@gmail.com>
2019-11-13 16:21:43 +0900

    commit initial

--- db/schema.rb
+++ db/schema.rb
@@ -95,8 +95,9 @@ ActiveRecord::Schema.define(version: 2019_11_06_100634) do
     t.integer "prefectures", null: false
     t.string "city", null: false
     t.string "house_number", null: false
-    t.string "building_name", null: false
-    t.string "phone_number", null: false
+    t.string "building_name"
+    t.string "phone_number"
+    t.string "phone_number_deliver"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
