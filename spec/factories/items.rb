610c0d09084f1c47c0acab5aa623438da0165b5b
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-14 18:43:08 +0900

    correction to factoryBot items.rb

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -11,17 +11,3 @@ FactoryBot.define do
     user_id           {1}
   end
 end
-
-# FactoryBot.define do
-#   factory :item do
-#     price           {1000}
-#     description     {"美味しいバナナです。"}
-#     title           {"バナナ"}
-#     status          {1}
-#     shipping_charge {1}
-#     shipping_method {1}
-#     delivery_source {13}
-#     shipping_day    {2}
-#     user_id         {1}
-#   end
-# end
